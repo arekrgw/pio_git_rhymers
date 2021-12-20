6ea64b20b0026797cec20623936c256f669bdc9e
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2021-12-20 11:30:15 +0100

    1.4 stale 1 zmiana liczb na deklaracje stalych

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,7 +2,9 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	public static final int size = 12;
+	public static final int returnError = -1;
+	private int[] numbers = new int[size];
 
 	public int total = -1;
 
@@ -21,14 +23,14 @@ public class DefaultCountingOutRhymer {
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return returnError;
 
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return returnError;
 
 		return numbers[total--];
 	}
