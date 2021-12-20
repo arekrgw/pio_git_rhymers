e4a1100a55c54ac3de96c6e655b336f2e411f8a6
ArekRGW <arkadiusz.pawlak.web@gmail.com>
2021-12-20 11:36:47 +0100

    1.4 accesory 1 class fields visibility change

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,13 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	public static final int size = 12;
-	public static final int returnError = -1;
-	public static final int totalInitialValue = -1;
+	private static final int size = 12;
+	private static final int returnError = -1;
+	private static final int totalInitialValue = -1;
 	private int[] numbers = new int[size];
 
-
-	public int total = totalInitialValue;
+	private int total = totalInitialValue;
 
 	public void countIn(int in) {
 		if (!isFull())
